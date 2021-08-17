8481c3a5e84238e64cc5bac695484f368bdd5333
Kevin Kim <kwkim@student.unimelb.edu.au>
2021-08-17 23:46:17 +1000

    change redis url to match service name on kubernetes cluster

--- config/redis.go
+++ config/redis.go
@@ -7,7 +7,7 @@ import (
 var Redis *redis.Client
 
 func init() {
-	opt, err := redis.ParseURL("redis://localhost:6379/0")
+	opt, err := redis.ParseURL("redis://redis-service:6379/0")
 	if err != nil {
 		panic(err)
 	}
