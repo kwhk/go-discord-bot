7c6c44abe3fcb6245a26cc42a8a73ff4c4da8b42
Kevin Kim <kwkim@student.unimelb.edu.au>
2021-08-17 23:18:30 +1000

    add redis client

---
+++ config/redis.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"github.com/go-redis/redis/v8"
+)
+
+var Redis *redis.Client
+
+func init() {
+	opt, err := redis.ParseURL("redis://localhost:6379/0")
+	if err != nil {
+		panic(err)
+	}
+
+	redis := redis.NewClient(opt)
+	Redis = redis
+}
